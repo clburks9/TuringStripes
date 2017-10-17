010f1d85b6484744261afde0de7f551a0c3941b9
Luke Burks <clburks9@gmail.com>
2017-10-17 09:08:11 -0600

    intial commit

---
+++ src/main.py
@@ -0,0 +1,28 @@
+'''
+************************************************************************************************************************************************************
+File: main.py
+Written By: Luke Burks
+October 2017
+
+Coding up the partial differential equations governing 
+Reaction-Diffusion systems as described by Alan Turing
+
+
+************************************************************************************************************************************************************
+'''
+
+
+
+__author__ = "Luke Burks"
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Luke Burks"
+__email__ = "clburks9@gmail.com"
+__status__ = "Development"
+
+
+
+
+
+if __name__ == '__main__':
+	print('hi'); 
